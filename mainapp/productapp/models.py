f6c0d6e7d767190ef9a871a9034e53d2571f6182
Eugene <evgeshka120@gmail.com>
2022-06-02 14:18:18 +0300

    product model

--- mainapp/productapp/models.py
+++ mainapp/productapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    rcv_date = models.DateTimeField()
+    price = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=1)
+    vendor_name = models.CharField(max_length=64)
